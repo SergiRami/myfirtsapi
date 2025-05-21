2a4646f703b5ecf7d56039b6ce7672b31d4a2075
SergiRami <sramirez107473@umanizales.edu.co>
2025-05-21 16:56:38 -0500

    Mi primer proyecto

--- src/main/java/co/edu/umanizales/myfirtsapi/controller/StoreController.java
+++ src/main/java/co/edu/umanizales/myfirtsapi/controller/StoreController.java
@@ -4,9 +4,7 @@ package co.edu.umanizales.myfirtsapi.controller;
 import co.edu.umanizales.myfirtsapi.model.Store;
 import co.edu.umanizales.myfirtsapi.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,9 @@ public class StoreController {
     @GetMapping
     public List<Store> getStores() {
         return storeService.getStores();
+    }
+    @PostMapping
+    public String createStore(@RequestBody Store store) {
+        return storeService.addStore(store);
     }
 }
--- src/main/java/co/edu/umanizales/myfirtsapi/service/StoreService.java
+++ src/main/java/co/edu/umanizales/myfirtsapi/service/StoreService.java
@@ -3,18 +3,24 @@ package co.edu.umanizales.myfirtsapi.service;
 
 import co.edu.umanizales.myfirtsapi.model.Store;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @Service
 @Getter
 public class StoreService {
+    @Autowired
     private List<Store> stores;
 
+    @GetMapping
     public String addStore(Store store) {
         stores.add(store);
         return "Tienda adicionada";
 
     }
+
+
 }
